3bd549ee961b6d20ff61d32c65076cdc7906035e
ljh9 <dkssud2208@naver.com>
2023-04-25 01:48:44 +0900

    Update TestServer

--- server.ts
+++ server.ts
@@ -1,3 +1,20 @@
-import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
+// import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
 
-serve((_req) => new Response("Hello, world"), { port: 5000 });
+// serve((_req) => new Response("Hello, world"), { port: 5000 });
+
+
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get('/',(context) => {
+    context.response.body = "Hello ~~~";
+})
+
+console.log(`port 5000`);
+await app.listen({ port : 5000 });
